a2e15e4b53aaf1aaf1b59c1af1e35e083c4301cc
Zergity <Zergity@gmail.com>
2018-08-24 16:43:03 +0700

    Use -f for first time setup.

--- simnet.sh
+++ simnet.sh
@@ -7,14 +7,18 @@ OPTIND=1         # Reset in case getopts has been used previously in the shell.
 # Initialize our own variables:
 remove=0
 daemon_only=0
+first=0
 
-while getopts "h?rd" opt; do
+while getopts "h?frd" opt; do
     case "$opt" in
     h|\?)
-        echo "$(basename ""$0"") [-h] [-?] [-r] [-d] nodes_count"
+        echo "$(basename ""$0"") [-h] [-?] [-f|-r] [-d] nodes_count"
         exit 0
         ;;
 	r)	remove=1
+		;;
+	f)	first=1
+		remove=1
 		;;
 	d)	daemon_only=1
 		;;
@@ -28,12 +32,20 @@ shift $((OPTIND-1))
 # addresses and keys
 MINING_ADDR=SUkMuEFAq5MgzrdFghNF6LB9N5W8e74Q81
 MINING_SKEY=Fs2ezDkpassKCSG1UpDqcV2ib1sC5NQNgAZRBsgk2Xgwj7jxLrk3
+WALLET_ADDR=SSwMMZKdfuK7oPjhEGuzVPtGVuGQfaG6Tb
+WALLET_SKEY=FudTNM3XSmTHzxHVkHHXHGAidcaYACK2hKiVLtZAmsuELsf7xShq
+
+if [[ $first -ne 0 ]]; then
+	IMPORT_SKEY=$MINING_SKEY
+else
+	IMPORT_SKEY=$WALLET_SKEY
+fi
 
 # process OPTs
 if [[ $remove -ne 0 ]]; then
 	rm -rf "$LOCALAPPDATA/btcd/data/simnet"
-	rm -rf "$LOCALAPPDATA/btcwallet/simnet"
 fi
+rm -rf "$LOCALAPPDATA/btcwallet/simnet"
 
 start btcd --simnet --rpcuser=a --rpcpass=a --miningaddr=$MINING_ADDR
 
@@ -44,6 +56,17 @@ if [[ $daemon_only -eq 0 ]]; then
 
 	sleep 5
 
-	btcctl --simnet --rpcuser=a --rpcpass=a --skipverify --wallet walletpassphrase "password" 0 &&\
-	btcctl --simnet --rpcuser=a --rpcpass=a --skipverify --wallet importprivkey $MINING_SKEY
+	btcctl --simnet --rpcuser=a --rpcpass=a --skipverify --wallet walletpassphrase "password" 0
+	btcctl --simnet --rpcuser=a --rpcpass=a --skipverify --wallet importprivkey $IMPORT_SKEY
+	btcctl --simnet --rpcuser=a --rpcpass=a --skipverify --wallet settxfee 0
+
+	if [[ $first -ne 0 ]]; then
+		btcctl --simnet --rpcuser=a --rpcpass=a --skipverify generate 101
+
+		sleep 2
+
+		btcctl --simnet --rpcuser=a --rpcpass=a --skipverify --wallet sendfrom imported $WALLET_ADDR 50
+		btcctl --simnet --rpcuser=a --rpcpass=a --skipverify --wallet sendfrom1 imported $WALLET_ADDR 1
+		btcctl --simnet --rpcuser=a --rpcpass=a --skipverify generate 1
+	fi
 fi
